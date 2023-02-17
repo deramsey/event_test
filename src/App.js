5e9212ca6d3743a91e23ad2bb7dc19e48921e424
deramsey <ramseyd@clevelandcc.edu>
2023-02-17 07:51:29 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return(
+    <div>
+    <h1>{count}</h1><br/>
+    <button onClick = {() => setCount(count+2) }>Click Me to Increment</button>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
